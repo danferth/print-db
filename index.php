24dabb8a63e55cfa42175e1f2a2680f2065f31d2
Dan Klotz <danferth@gmail.com>
2013-09-26 15:38:07 -0700

    added favicon support

--- index.php
+++ index.php
@@ -12,6 +12,19 @@ include "assets/connection.php";
 <head>
 	<meta charset="UTF-8">
 	<title>Log|In</title>
+	<!-- Opera speed dial icon -->
+    <link rel="icon" type="image/png" href="assets/icons/195x195image.png">
+	<!-- display largest first as iOS < 4.2 does not support size attr -->
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/icons/apple-touch-icon-114x114-precomposed.png">
+	<!-- For iPad: apple doesn't need this but andoid OS (2.1+) does -->
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/icons/apple-touch-icon-72x72-precomposed.png">
+    <link rel="apple-touch-icon" href="assets/icons/touch-icon-iphone.png" />
+	<!-- browser should find ICO file by default without <link> -->
+	<link rel="icon" type="image/png" href="assets/icons/favicon.png" />
+	<!--[if IE]><link rel="shortcut icon" href="pathto/favicon.ico"><![endif]-->
+	<!-- or, set /favicon.ico for IE10 win -->
+	<meta name="msapplication-TileColor" content="#2E8D61">
+	<meta name="msapplication-TileImage" content="assets/icons/tileicon.png">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/login.css">
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
